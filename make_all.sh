457d0cf601f5ccf61bf204675ad8f748c4a2ac79
Chris Berry <chris.berry@getcruise.com>
2018-10-09 15:12:22 -0700

    update location of linux headers

--- make_all.sh
+++ make_all.sh
@@ -1,10 +1,10 @@
 # Need to use the exact version of gcc that was used to compile the kernel
 # It's in /usr/bin, so put it at the front of the path
 export PATH=/usr/bin:$PATH
-SRC_PATH=/usr/src/kernels/`uname -r`-`uname -m`
+SRC_PATH=/usr/src/linux-headers-`uname -r`-`uname -m`
 if [ ! -d "$SRC_PATH" ]
 then
-	SRC_PATH=/usr/src/kernels/`uname -r`
+	SRC_PATH=/usr/src/linux-headers-`uname -r`
 fi
 echo "Using kernel source files from  $SRC_PATH"
 
